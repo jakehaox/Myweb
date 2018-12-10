c242edd751c85b60e5e7465a97f58a0ddd0857c7
haoxinning <954337612@qq.com>
2018-12-10 17:05:50 +0800

    allproject

---
+++ 004.JS/js/004-js样式.js
@@ -0,0 +1,6 @@
+function toChange(){
+			var toBig=document.gitElementById('box');
+			toBig.style.width='200';
+			toBig.style.height='200';
+			toBig.style.background='blue';
+		}
---
+++ 004.JS/js/005-js提取事件.js
@@ -0,0 +1,8 @@
+function toChange(){
+	var toSmall = document.getElementById('box');
+	toSmall.style.width = '50px';
+	toSmall.style.height = '50px';
+	toSmall.style.background = 'blue';
+}
+   var toSmall = document.getElementById('box');
+   box.onclick = toChange;
