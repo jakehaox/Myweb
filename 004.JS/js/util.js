cfe2e95dfb31ddca16496c9367b535594b1b76cb
haoxinning <954337612@qq.com>
2018-12-11 14:19:12 +0800

    1

--- 004.JS/js/util.js
+++ 004.JS/js/util.js
@@ -1,31 +1,43 @@
 
-    function animate(obj,attr,target){
-	    clearInterval(obj.timer);
-	    var iSpend = 0;
-	    obj.timer = setInterval(function(){
-		    var current = parseFloat(getComputedStyle(obj,false)[attr]);
-		    if(attr == 'opacity'){
-		    	  current = Math.round(current * 100);
-		    }
-		    if(current < target){
-		    	iSpend = 10;
-		    }else{
-		    	iSpend = -10;
-		    }
-		    if(Math.abs(target - current) == Math.abs(iSpend)){
-		    	if(attr == 'opacity'){
-		    		obj.style.opacity = target / 100;
-		    	}else{
-		    		obj.style[attr] = target + 'px';
-		    	}
-		    	clearInterval(obj.timer);
-		    }else{
-		    	if(attr == 'opacity'){
-		    	   obj.style.opacity = (current + iSpend) / 100;	
-		    	}else{
-		    		obj.style[attr] = current + iSpend + 'px';
-		    	}
-		      }
-		    },30);
-	    // console.log(obj,attr,target);
-	    }
+    
+/**
+ * @param  {object} obj    [DOM节点对象]
+ * @param  {string} attr   [属性名称]
+ * @param  {number} target [目标值]
+ */
+function animate(obj,attr,target){
+	clearInterval(obj.timer);
+	var iSpeed = 0;
+	obj.timer = setInterval(function(){
+		var current = parseFloat(getComputedStyle(obj,false)[attr]);
+		if(attr == 'opacity'){
+			current = Math.round(current * 100);
+		}
+		if(current > target){
+			iSpeed = -80;
+		}else{
+			iSpeed = 80;
+		}
+		if(Math.abs(target - current) < Math.abs(iSpeed)){
+			if(attr == 'opacity'){
+				obj.style.opacity = target / 100;
+			}else{
+				obj.style[attr] = target + 'px';
+			}
+			clearInterval(obj.timer);
+		}else{
+			if(attr == 'opacity'){
+				obj.style.opacity = (current + iSpeed)/100;	
+			}else{
+				obj.style[attr] = current + iSpeed + 'px';
+			}
+		}
+	},30)
+}
+
+
+function getScrollTop(){
+	return  window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;	
+}
+
+
