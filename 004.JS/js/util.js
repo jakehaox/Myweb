f37d51590d839f16a38222cb5849f80ac887877d
haoxinning <954337612@qq.com>
2018-12-12 15:39:07 +0800

    2

--- 004.JS/js/util.js
+++ 004.JS/js/util.js
@@ -94,6 +94,10 @@ function getScrollTop(){
 	return  window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;	
 }
 
+function getScrollLeft(){
+	return  window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;	
+}
+
 
 
 
