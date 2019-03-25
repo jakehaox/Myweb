13ac2b88e15339763c8ebbe546eb99e5bfac0622
haoxinning <954337612@qq.com>
2019-03-25 11:48:47 +0800

    3

--- 8.Nodejs/07-http/04.post/server.js
+++ 8.Nodejs/07-http/04.post/server.js
@@ -1,10 +1,27 @@
 
 
 const http = require('http');
-const fs = require('fs');
-const host
-const server = http.createServer((req,res)=>{
-	console.log('url=>',req.url);
-	const filePath = req.url;
+const querystring = require('querystring');
 
-       
+const server = http.createServer((req,res)=>{
+	
+	console.log('url=>',req.url,'method=>',req.method);
+
+	let body = '';
+	req.on('data',(chunk)=>{
+		body += chunk;
+	})
+
+	req.on('end',()=>{
+		// console.log(body);
+		let obj = querystring.parse(body);
+		console.log(obj);
+	})
+
+	res.setHeader('Content-Type',"text/html;charset=utf-8");
+	res.end('kuazhu');
+});
+
+server.listen(3000,'127.0.0.1',()=>{
+	console.log('server is running at http://127.0.0.1:3000')
+})      
--- 8.Nodejs/09-dreamwish/static/js/index.js
+++ 8.Nodejs/09-dreamwish/static/js/index.js
@@ -11,7 +11,6 @@
 		return Math.round(min + (max-min)*Math.random());
 	}
 	
-	
 	var $wall = $('.wall');
 	var $wish = $('.wish');
 	//获取父容器和自身宽高
@@ -21,11 +20,15 @@
 		wishHeight = $wish.height();
 	//1.设置许愿卡片拖动
 	$wish.pep({  constrainTo: '.wall' })
+
 	$wish.each(function(){
-		let x = (0,wallWidth-wishWidth);
-		let y = (0,wallHeight-wishHeight);
+		let x = getRandom(0,wallWidth - wishWidth);
+		let y = getRandom(0,wallHeight - wishHeight);
+		$(this).css({
+			transform: "matrix(1, 0, 0, 1, "+x+", "+y+")"
+		})
 	})
-	transform:matrix()
+
 
 
 
