28af8d3b2e8521ee70a0e9a0cac7eac50403c0ad
haoxinning <954337612@qq.com>
2019-04-12 19:30:17 +0800

    34

---
+++ 02-react/06-todolist-react-redux/src/store/index.js
@@ -0,0 +1,23 @@
+
+import { createStore,applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer from './reducer.js'
+
+//1.store是负责整个数据的管理(获取最新的state,派发action,监听state的改变)
+//2.整个应用只有一个store
+//3.创建store时第一个参数需要传入一个函数(reducer)
+
+// const store = createStore(reducer)
+
+// console.log(process.env.NODE_ENV)
+const middleware = [thunk]
+
+if(process.env.NODE_ENV != 'production'){
+	const logger = createLogger({});
+	middleware.push(logger)
+}
+
+const store = createStore(reducer,applyMiddleware(...middleware))
+
+export default store
